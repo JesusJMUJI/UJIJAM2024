8727c24b99850b33cc37018c356c5402b6b9bbc4
Karl <lvlthreemage@gmail.com>
2024-02-03 14:28:47 +0100

    Update LevelManagerShortcut.cs

--- Assets/LevelManagerShortcut.cs
+++ Assets/LevelManagerShortcut.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 public class LevelManagerShortcut : MonoBehaviour
 {
-	LevelManager levelManager;
-	void Start(){
-		levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
-	}
 	public void LoadLevel(int levelIndex){
 		LevelManager.instance.LoadScene(levelIndex);
 	}
