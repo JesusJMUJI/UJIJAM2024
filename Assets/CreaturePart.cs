76bce96d381245a20f3690a993c4adfeceefb5c6
Karl <lvlthreemage@gmail.com>
2024-02-04 02:35:08 +0100

    Update CreaturePart.cs

--- Assets/CreaturePart.cs
+++ Assets/CreaturePart.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class CreaturePart : MonoBehaviour
 {
-    PartAsset asset;
-    public void AssignAsset(PartAsset _asset){
-        asset = _asset;
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	float health;
+	PartAsset asset;
+	public void AssignAsset(PartAsset _asset){
+		asset = _asset;
+		health = asset.health;
+	}
+	//On collison
+		//if colliding with another creature part
+		//Damage other creature part by damage
+	
+	//Damage method
+		//if health > 0
+		//reduce health by damage
+		//if health == 0
+			//destroy gameObject
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
 }
